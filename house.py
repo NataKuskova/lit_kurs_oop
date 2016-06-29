efd7f25087bb180d4a57030681d178dbf988e04f
Natali <natasha.kuskova@gmail.com>
2016-06-30 02:06:55 +0300

    update

--- house.py
+++ house.py
@@ -1,33 +1,44 @@
-class VolumeMixin(object):
-
-    def volume(self):
-        pass
-
-
-class WeightMixin(object):
-
-    def weight(self):
-        pass
-
-
-class Furniture(WeightMixin, object):
-    # мебель
-    material = None
+class Properties:
+    length = None
     width = None
     height = None
-    x = None
-    y = None
+    coordinate = {}
 
-    # def __init__(self, material, width, height):
     def __init__(self, **kwargs):
-        self.material = kwargs['material']
+        self.length = kwargs['length']
         self.width = kwargs['width']
         self.height = kwargs['height']
+        self.coordinate = kwargs['coordinate']
 
 
-class Appliance(Furniture, object):
+class Furniture(Properties):
+    # мебель
+    material = None
+    weight = None
+
+    def __init__(self, **kwargs):
+        self.material = kwargs['material']
+        self.weight = kwargs['weight']
+        super().__init__(**kwargs)
+
+
+class Appliance(Properties):
     # приборы
-    pass
+    energy_consumption = None
+    opening_hours = None  # часы работы
+    weight = None
+
+    def __init__(self, **kwargs):
+        self.energy_consumption = kwargs['energy_consumption']
+        self.opening_hours = kwargs['opening_hours']
+        self.weight = kwargs['weight']
+        super().__init__(**kwargs)
+
+    def energy_consumption_one(self):
+        day = self.energy_consumption * self.opening_hours
+        day /= 1000
+        month = day * 30
+        return month
 
 
 class Table(Furniture):
@@ -37,6 +48,21 @@ class Table(Furniture):
 
 class Chair(Furniture):
     # стул
+    pass
+
+
+class Cupboard(Furniture):
+    # шкаф
+    pass
+
+
+class Sofa(Furniture):
+    # диван
+    pass
+
+
+class TV(Furniture):
+    # телевизор
     pass
 
 
@@ -55,25 +81,53 @@ class Oven(Appliance):
     pass
 
 
-class Room(VolumeMixin, object):
+class Sink(Furniture):
+    # раковина
+    pass
+
+
+class Heater(Appliance):
+    # обогреватель
+    pass
+
+
+class Conditioner(Appliance):
+    # кондиционер
+    pass
+
+
+class WinDoor(Furniture):
+    # окна, двери
+    kind = None
+
+    def __init__(self, **kwargs):
+        self.kind = kwargs['kind']
+        super().__init__(**kwargs)
+
+
+class Room(Properties):
     furniture = None
-    length = None
-    width = None
-    height = None
-    x = None
-    y = None
     win_door = []
 
-    # def __init__(self, furniture, length, width, height, win_door):
     def __init__(self, **kwargs):
         self.furniture = kwargs['furniture']
-        self.length = kwargs['length']
-        self.width = kwargs['width']
-        self.height = kwargs['height']
         self.win_door = kwargs['win_door']
+        super().__init__(**kwargs)
 
     def volume(self):
         return self.length * self.width * self.height
+
+    def weight(self):
+        sum_ = sum([furniture.weight for furniture in self.furniture])
+        sum_ += sum([win_door.weight for win_door in self.win_door])
+        return sum_
+
+    def energy_consumption_month(self):
+        energy = 0
+        for furniture in self.furniture:
+            if isinstance(furniture, Appliance):
+                energy += furniture.energy_consumption_one()
+        return energy
 
 
 class Kitchen(Room):
@@ -91,15 +145,128 @@ class Hall(Room):
     pass
 
 
-class House(WeightMixin, VolumeMixin, object):
+class Corridor(Room):
+    # коридор
+    pass
+
+
+class House(Properties):
     rooms = None
 
     def __init__(self, **kwargs):
         self.rooms = kwargs['rooms']
+        super().__init__(**kwargs)
 
-    def volume(self):
-        return sum([room.volume for room in self.rooms])
+    def volume_house(self):
+        return sum([room.volume() for room in self.rooms])
+
+    def weight_house(self):
+        return sum([room.weight() for room in self.rooms])
+
+    def energy_consumption(self):
+        return sum([room.energy_consumption_month() for room in self.rooms])
 
 
 if __name__ == "__main__":
-    pass
+    house = House(
+        rooms=[
+            Kitchen(
+                furniture=[
+                    Refrigerator(
+                        energy_consumption=250,
+                        opening_hours=8,
+                        length=0.6,
+                        width=0.5,
+                        height=1.5,
+                        weight=70,
+                        coordinate={
+                            'left_bottom': (0, 1),
+                            'right_top': (0.6, 1.5)
+                        }
+                    ),
+                    Oven(
+                        energy_consumption=3000,
+                        opening_hours=4,
+                        length=0.5,
+                        width=0.5,
+                        height=0.5,
+                        weight=33,
+                        coordinate={
+                            'left_bottom': (2, 0),
+                            'right_top': (2.5, 0.5)
+                        }
+                    )
+                ],
+                length=4,
+                width=3,
+                height=3,
+                win_door=[
+                    WinDoor(
+                        kind='door',
+                        length=1,
+                        width=0.1,
+                        height=2,
+                        weight=50,
+                        material='wood',
+                        coordinate={
+                            'left_bottom': (4, 1),
+                            'right_top': (4, 2)
+                        }
+                    ),
+                    WinDoor(
+                        kind='window',
+                        length=1.5,
+                        width=0.2,
+                        height=1.5,
+                        weight=45,
+                        material='plastic',
+                        coordinate={
+                            'left_bottom': (0, 1),
+                            'right_top': (0, 2.5)
+                        }
+                    )
+                ],
+                coordinate={
+                    'left_bottom': (0, 0),
+                    'right_top': (4, 3)
+                }
+            ),
+            Corridor(
+                furniture='',
+                length=7,
+                width=2,
+                height=3,
+                win_door=[
+                    WinDoor(
+                        kind='door',
+                        length=1,
+                        width=0.1,
+                        height=2,
+                        weight=50,
+                        material='wood',
+                        coordinate={
+                            'left_bottom': (4.5, 0),
+                            'right_top': (5.5, 0)
+                        }
+                    )
+                ],
+                coordinate={
+                    'left_bottom': (4, 0),
+                    'right_top': (6, 7)
+                }
+            )
+        ],
+        length=10,
+        width=7,
+        height=3,
+        coordinate={
+            'left_bottom': (0, 0),
+            'right_top': (10, 7)
+        }
+    )
+    weight = house.weight_house()
+    print("The weight of house = " + str(weight) + " kg")
+    volume = house.volume_house()
+    print("The volume of house = " + str(volume) + " m^3")
+    energy = house.energy_consumption()
+    print("Energy consumption per month = " + str(energy) + " kW/h")
